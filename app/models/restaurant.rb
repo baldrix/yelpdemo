8bba4b8ccc02ec7fffb98efef323843f47773b70
Marco <marcoesparzaf@outlook.com>
2015-09-30 13:54:35 -0700

    Added som validations to restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,10 @@
 class Restaurant < ActiveRecord::Base
 	mount_uploader :image, ImageUploader
 	has_many :reviews
+
+	validates :name , :address,:phone , :website,:image,presence: true
+	validates :phone, format:{with: /\A\(\d{3}\) \d{3}-\d{4}\z/,
+					message: "must be in the format (123) 123-4567"}
+	validates :website ,format: {with: /\Ahttps?:\/\/.*\z/,
+					message:"must start with http://"}
 end
